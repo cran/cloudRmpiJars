7b8b92165babb943c7f3829d0c2a67c2bb0c5f8e
Barnet Wagman <bw@norbl.com>
2012-03-24 00:00:00 +0000

    version 1.0

---
+++ R/cloudRmpiJars-package.R
@@ -0,0 +1,22 @@
+#' Cloud-based MPI Parallel Proccessing for R (cloudRmpi)
+#'
+#' \tabular{ll}{
+#' Package: \tab cloudRmpiJars\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0--\cr
+#' Date: \tab 2012-01-24\cr
+#' License: \tab GPL (>= 3)\cr
+#' }
+#'
+#' This package contains third-party Java class jars used by cloudRmpi.
+#' cloudRmpi is means for doing parallel processing in R, using
+#' MPI on a cloud-based network.
+#'
+#' @name cloudRmpiJars
+#' @docType package
+#' @title Third-party jars for cloudRmpi
+#' @author Barnet Wagman \email{bw@@norbl.com}
+#' @depends
+#' @keywords package
+NULL
+           
